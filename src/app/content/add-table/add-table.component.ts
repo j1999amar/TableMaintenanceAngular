ed503be8ca267e67bd5f1cf9f5646b902b324515
j1999amar <j1999amar@gmail.com>
2023-08-30 12:45:57 +0530

    Header Footer Content Impl

--- src/app/content/add-table/add-table.component.ts
+++ src/app/content/add-table/add-table.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ApiService } from '../api.service';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-add-table',
